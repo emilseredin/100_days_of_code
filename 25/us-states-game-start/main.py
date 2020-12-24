69026b46ebce7bb32b3a8e006ba6904bdb5dfb5b
Emil Seredin <emils.seredins@gmail.com>
2020-12-24 17:44:21 +0200

    save the states that were not guessed in a csv file

--- 25/us-states-game-start/main.py
+++ 25/us-states-game-start/main.py
@@ -19,6 +19,16 @@ def parse_csv():
     return states
 
 
+def write_csv(data, filename):
+    with open(filename, "w") as f:
+        field_names = ["state"]
+        writer = csv.DictWriter(f, field_names)
+        writer.writeheader()
+        for state in data:
+            r = {"state": state}
+            writer.writerow(r)
+
+
 def play(screen):
     states = parse_csv()
     turtle = Turtle()
@@ -29,15 +39,20 @@ def play(screen):
     input_title = "Guess the state"
     while keep_going:
         answer = screen.textinput(title=input_title, prompt="Enter the state name (type 'q' to quit): ").lower()
-        if answer == "q":
-            keep_going = False
-        elif answer in states:
+        if answer in states:
             guessed_states += 1
             turtle.setx(states[answer]["x"])
             turtle.sety(states[answer]["y"])
             turtle.write(answer.capitalize())
+            states.pop(answer)
             input_title = "{}/50 States Correct".format(guessed_states)
-        
+        elif answer == "q":
+            keep_going = False
+        if guessed_states == 50:
+            keep_going = False
+            print("Congratulations! You correctly named all 50 states!")
+    write_csv(data=states, filename="states_to_learn.csv")
+    
 
 def main():
     screen = Screen()
